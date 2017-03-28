f67d1990c9be3d10b197f5259edd1e4d35203d84
carlo.maiorano <pasqualecarlo.maiorano@yahoo.it>
2017-03-28 15:58:42 +0200

    added mock password

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/service/impl/WalletServiceImpl.java
+++ cakeshop-api/src/main/java/com/jpmorgan/cakeshop/service/impl/WalletServiceImpl.java
@@ -67,7 +67,7 @@ public class WalletServiceImpl implements WalletService, GethRpcConstants {
 
     @Override
     public Account create() throws APIException {
-        Map<String, Object> result = gethService.executeGethCall("personal_newAccount", new Object[]{""});
+        Map<String, Object> result = gethService.executeGethCall("personal_newAccount", new Object[]{"ciao"});
         String newAddress = (String) result.get("_result");
 
         Account account = new Account();
